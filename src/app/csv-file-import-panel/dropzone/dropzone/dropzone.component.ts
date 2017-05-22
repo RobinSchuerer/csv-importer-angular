807d0063870f6cec72c1cf4ce67b56a2065202bb
Robin Schürer <robin.schuerer@gmail.com>
2017-05-22 16:16:03 +0200

    cryptojs and file chunk upload

--- src/app/csv-file-import-panel/dropzone/dropzone/dropzone.component.ts
+++ src/app/csv-file-import-panel/dropzone/dropzone/dropzone.component.ts
@@ -1,7 +1,10 @@
 import {AfterViewInit, Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {ReplaySubject} from 'rxjs/ReplaySubject';
-import {ProgressbarComponent} from '../progressbar/progressbar.component';
+
+/// <reference path="node_modules/@types/crypto-js/index.d.ts" />
+import * as CryptoJS from 'crypto-js';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-dropzone',
@@ -9,6 +12,8 @@ import {ProgressbarComponent} from '../progressbar/progressbar.component';
   styleUrls: ['./dropzone.component.css']
 })
 export class DropZoneComponent implements OnInit {
+
+  private CHUNK_SIZE = 1024;
 
   private fileObserver: ReplaySubject<File> = new ReplaySubject<File>();
 
@@ -29,15 +34,55 @@ export class DropZoneComponent implements OnInit {
 
     event.preventDefault();
     const file: File = event.dataTransfer.files[0];
-    console.log('drop file: ' + file.name + ' with type: ' + file.type);
 
-    this.fileObserver.next(file);
-    // this.upload(file).subscribe((result) => console.log(result));
+    let start = 0;
+    const fileStop = file.size - 1;
+    let stop = 1000;
+
+    const reader = new FileReader();
+
+    // If we use onloadend, we need to check the readyState.
+
+    reader.onloadend = (progevent: ProgressEvent) => {
+      // console.log(reader.result);
+      //
+
+      if (reader.readyState === 2) {
+        const key = CryptoJS.enc.Base64.parse('HackersSeeIT2');
+        const iv = CryptoJS.enc.Base64.parse('#base64IV#');
+        const message = reader.result;
+
+        // console.log(message);
+
+        const encrypted = CryptoJS.AES.encrypt(message, key, {iv: iv});
+
+        console.log(encrypted.ciphertext);
+
+        start = start + this.CHUNK_SIZE;
+        this.seek(reader, file, start);
+      }
+    }
+
+    reader.onload = (progress: ProgressEvent) => console.log(Math.round(progress.loaded / progress.total * 100));
+
+    this.seek(reader, file, 0);
 
   }
 
   public onFileSelection(): Observable<File> {
     return this.fileObserver;
+  }
+
+  private seek(fileReader: FileReader, file: File, offset: number) {
+    if (offset >= file.size) {
+      // No \r or \n found. The column size is equal to the full
+      // file size
+      console.log('end');
+      return;
+    }
+
+    const slice = file.slice(offset, offset + this.CHUNK_SIZE);
+    fileReader.readAsBinaryString(slice);
   }
 
 
